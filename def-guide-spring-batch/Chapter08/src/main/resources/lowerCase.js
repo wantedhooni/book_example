8e7078ea9729bcaf23a106b3d3624217f1bc5e26
Revy <wantedhooni@gmail.com>
2026-04-19 16:47:06 +0900

    Update .gitignore, 9781484237236.jpg, .gitignore, and 254 more files

--- def-guide-spring-batch/Chapter08/src/main/resources/lowerCase.js
+++
@@ -1,4 +0,0 @@
-item.setAddress(item.getAddress().toLowerCase());
-item.setCity(item.getCity().toLowerCase());
-item.setState(item.getState().toLowerCase());
-item;
--- def-guide-spring-batch/Chapter08/src/main/resources/upperCase.js
+++
@@ -1,4 +0,0 @@
-item.setFirstName(item.getFirstName().toUpperCase());
-item.setMiddleInitial(item.getMiddleInitial().toUpperCase());
-item.setLastName(item.getLastName().toUpperCase());
-item;
