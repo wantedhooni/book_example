b9dea0ce434d6101fc104905c91b4e25fed3b07b
Revy <wantedhooni@gmail.com>
2026-04-25 18:13:39 +0900

    Update .gitignore, jvm.config, maven.config, and 35 more files

---
+++ spring/spring-cloud-circuitbreaker-demo/spring-cloud-circuitbreaker-demo-reactive-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/reactiveresilience4jcircuitbreakerdemo/ReactiveResilience4JCircuitbreakerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package org.springframework.cloud.circuitbreaker.demo.reactiveresilience4jcircuitbreakerdemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class ReactiveResilience4JCircuitbreakerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ spring/spring-cloud-circuitbreaker-demo/spring-cloud-circuitbreaker-demo-resilience4j/src/test/java/org/springframework/cloud/circuitbreaker/demo/resilience4jcircuitbreakerdemo/Resilience4JCircuitbreakerDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package org.springframework.cloud.circuitbreaker.demo.resilience4jcircuitbreakerdemo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Resilience4JCircuitbreakerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
